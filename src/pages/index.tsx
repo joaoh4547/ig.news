3528949a731fa7c3c57447244df67323a61894b1
João Henrique <joaohenriquetdj@gmail.com>
2021-12-05 14:01:11 -0300

    add SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import styles from './home.module.scss'
 import { stripe } from '../services/stripe'
 
@@ -34,7 +34,7 @@ export default function Home({ product }: HomeProps) {
     )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
     const price = await stripe.prices.retrieve('price_1K3MaQDrYljZFgQKifzRgj5C', {
 
     })
@@ -46,6 +46,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
     return {
         props: {
             product
-        }
+        },
+        revalidate: 60 * 60 * 24 // 24 Hours
     }
 }
