6f5e68a9c5a1e7aefb754ea84a3b041f9925e3b2
João Henrique <joaohenriquetdj@gmail.com>
2021-12-09 13:29:27 -0300

    add update fauna query

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -11,16 +11,30 @@ export default NextAuth({
             scope: "read:user",
         }),
     ],
-    jwt: {
-        signingKey: process.env.JWT_KEY,
-    },
+
     callbacks: {
         async signIn(user, acount, profile) {
             try {
                 const { email } = user;
 
                 await fauna.query(
-                    q.Create(q.Collection("users"), { data: { email } })
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index("user_by_email"),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(q.Collection("users"), { data: { email } }),
+                        q.Get(
+                            q.Match(
+                                q.Index("user_by_email"),
+                                q.Casefold(user.email)
+                            )
+                        )
+                    )
                 );
 
                 return true;
