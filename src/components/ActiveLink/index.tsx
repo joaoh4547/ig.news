2be38fcf5e94e53958741fec9721cf5aea8087e8
João Henrique <joaohenriquetdj@gmail.com>
2021-12-09 22:44:25 -0300

    add ActiveLink component

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,19 @@
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+import { ReactElement, cloneElement } from "react";
+
+interface ActiveLinkProps extends LinkProps {
+    children: ReactElement;
+    activeClassname;
+}
+
+export function ActiveLink({
+    children,
+    activeClassname,
+    ...rest
+}: ActiveLinkProps) {
+    const { asPath } = useRouter();
+
+    const className = asPath === rest.href ? activeClassname : "";
+    return <Link {...rest}>{cloneElement(children, { className })}</Link>;
+}
