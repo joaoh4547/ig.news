6ba8bc979e96ded9b85c207952feea365fef151c
João Henrique <joaohenriquetdj@gmail.com>
2021-12-09 21:33:31 -0300

    add create posts page and styling page

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import styles from "./styles.module.scss";
+
+export default function Posts() {
+    return (
+        <>
+            <Head>
+                <title>Posts | IgNews</title>
+            </Head>
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    <a href="">
+                        <time>12 de março de 2021</time>
+                        <strong>Titulo do post</strong>
+                        <p>Breve paragrafo</p>
+                    </a>
+                    <a href="">
+                        <time>12 de março de 2021</time>
+                        <strong>Titulo do post</strong>
+                        <p>Breve paragrafo</p>
+                    </a>
+                    <a href="">
+                        <time>12 de março de 2021</time>
+                        <strong>Titulo do post</strong>
+                        <p>Breve paragrafo</p>
+                    </a>
+                </div>
+            </main>
+        </>
+    );
+}
